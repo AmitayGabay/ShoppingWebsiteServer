903e3fb9b58fb6b88d542ab8e738caf55e402112
Amitay <amitaygabay1@gmail.com>
2023-04-09 16:19:46 +0300

    first commit

---
+++ src/main/java/com/example/ShoppingWebsiteServer/ShoppingWebsiteServerApplication.java
@@ -0,0 +1,13 @@
+package com.example.ShoppingWebsiteServer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShoppingWebsiteServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ShoppingWebsiteServerApplication.class, args);
+	}
+
+}
